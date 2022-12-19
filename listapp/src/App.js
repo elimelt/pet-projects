b964e58887aad5d6c2fd8882f52c33a223527d6e
elimelt <elimelt@uw.edu>
2022-12-19 12:33:32 -0800

    added files for note app

---
+++ listapp/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const App = (props) => {
+  const { notes } = props
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <li>{note.content}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
