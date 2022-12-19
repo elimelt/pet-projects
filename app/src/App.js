f198176facc1b3682c12914603c29b7c37242df4
elimelt <elimelt@uw.edu>
2022-12-18 16:13:20 -0800

    feedback app from react course

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,67 @@
 import { useState } from 'react'
 
+const Header = (props) => 
+  <><h1>{props.text}</h1></>
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, name}) => 
-  <button onClick={onClick}> {name}</button>
-
-const History = (props) => {
-  if (props.allClicks.length === 0) return <div>push a button</div>
-  else return <div>history: {props.allClicks.join(' ')}</div> 
+const Button = ({ handleClick, text}) => {
+  return (<><button onClick={handleClick}>{text}</button></>)
 }
 
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClicks = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClicks = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
+const Feedback = ({ hText, clicks, texts, i }) =>{
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClicks} name='left'/>
-      <Button onClick={handleRightClicks} name='right'/>
-      {right}
-      <History allClicks={allClicks}/>
+      <Header text={hText}/>
+      <Button handleClick={clicks[0]} text={texts[0]}/>
+      <Button handleClick={clicks[1]} text={texts[1]}/>
+      <Button handleClick={clicks[2]} text={texts[2]}/>
     </div>
   )
 }
-export default App;
+
+const Stats = ({ hText, g, b, n}) => {
+  if (g + b + n === 0) return <><p>no reviews</p></>
+  return( 
+    <div>
+      <Header text={hText}/>
+      <table>
+        <tbody>
+          <tr><StatLine name="good" stat={g}/></tr>
+          <tr><StatLine name="neutral" stat={n}/></tr>
+          <tr><StatLine name="bad" stat={b}/></tr>
+          <tr><StatLine name="all" stat={g + n + b}/></tr>
+          <tr><StatLine name="average" stat={(Math.round(100*(g-b)/(g + b + n))/100)}/></tr>
+          <tr><StatLine name="positive" stat={(Math.round(g/(g + b + n)*100))/100 + "%"}/></tr>
+        </tbody>
+      </table>
+      
+    </div>
+  )
+}
+
+const StatLine = ({ name, stat }) => {
+  return (
+    <><td>{name}</td><td>{stat}</td></>
+  )
+} 
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const clicks = [
+   () => setGood(good + 1), 
+   () => setNeutral(neutral + 1), 
+   () => setBad(bad + 1)
+  ]
+  const texts = ['good', 'neutral', 'bad']
+  return (
+    <div>
+      <Feedback hText="give feedback" clicks={clicks} texts={texts}/>
+      <Stats hText="statistics" g={good} n={neutral} b={bad}/>
+      
+    </div>
+  )
+}
+
+export default App
