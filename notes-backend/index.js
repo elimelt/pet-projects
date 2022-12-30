4b2c5c59fed57458cda5c91b8c00f9263bd63c30
elimelt <elimelt@uw.edu>
2022-12-29 21:02:31 -0800

    added backend for notes app

---
+++ notes-backend/index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+app.get('/', (req, res) => {
+  
+  res.send('<h1>Hello, world!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+  
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.id)
+  notes = notes.filter(note => note.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  if (!body.content) 
+    return response
+      .status(400)
+      .json({error: 'content missing'})
+
+  const note = {
+    content: body.content, 
+    important: body.important || false, 
+    date: new Date(),
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+
+  console.log('note', note)
+  console.log('notes', notes)
+  
+  res.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
